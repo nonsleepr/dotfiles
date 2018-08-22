6cd0667a00801d3ae15a38e215de194b32287748
Alexander Bessonov <nonsleepr@gmail.com>
2018-08-22 09:14:12 -0400

    Hardcoded fzf path
    The previos version wasn't working.
    FIXME: Remove hardcode, make it portable.

--- system/fzf.zsh
+++ system/fzf.zsh
@@ -1,4 +1,4 @@
 export FZF_DEFAULT_COMMAND='ag -g ""'
-SHELL_DIR=$(dirname $(readlink -f $(which fzf)))/../shell
+SHELL_DIR=/home/linuxbrew/.linuxbrew/opt/fzf/shell
 source $SHELL_DIR/completion.zsh
 source $SHELL_DIR/key-bindings.zsh
