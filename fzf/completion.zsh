cba0b5c2306ec29abb4d89a19779d1ea74fe1fa1
Alexander Bessonov <nonsleepr@gmail.com>
2018-09-10 15:40:00 -0400

    Proper zsh settings order for fzf completion to work

---
+++ fzf/completion.zsh
@@ -0,0 +1,3 @@
+SHELL_DIR=$(brew --prefix)/opt/fzf/shell
+source $SHELL_DIR/completion.zsh
+source $SHELL_DIR/key-bindings.zsh
--- system/fzf.zsh
+++
@@ -1,4 +0,0 @@
-export FZF_DEFAULT_COMMAND='ag -g ""'
-SHELL_DIR=/home/linuxbrew/.linuxbrew/opt/fzf/shell
-source $SHELL_DIR/completion.zsh
-source $SHELL_DIR/key-bindings.zsh
