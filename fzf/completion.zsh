3071a87a944f85b189eb9c2fd39d0d29de9d6216
Alexander Bessonov <nonsleepr@gmail.com>
2019-02-08 15:06:09 -0500

    Handle fzf installed by something else than brew

--- fzf/completion.zsh
+++ fzf/completion.zsh
@@ -1,3 +1,9 @@
-SHELL_DIR=$(brew --prefix)/opt/fzf/shell
-source $SHELL_DIR/completion.zsh
-source $SHELL_DIR/key-bindings.zsh
+if hash brew 2>/dev/null; then
+  SHELL_DIR=$(brew --prefix)/opt/fzf/shell
+  source $SHELL_DIR/completion.zsh
+  source $SHELL_DIR/key-bindings.zsh
+else
+  FZF_DIR=$(dirname $(command -v fzf))/../share/fzf
+  source $FZF_DIR/completion.zsh
+  source $FZF_DIR/key-bindings.zsh
+fi
