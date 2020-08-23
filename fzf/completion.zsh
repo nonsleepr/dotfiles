75b34528e00305d48c2fe967575d2785eb5712e8
Alexander Bessonov <nonsleepr@gmail.com>
2020-08-23 02:54:29 -0400

    Add other paths for FZF

--- fzf/completion.zsh
+++ fzf/completion.zsh
@@ -2,6 +2,9 @@ if hash brew 2>/dev/null; then
   SHELL_DIR=$(brew --prefix)/opt/fzf/shell
   source $SHELL_DIR/completion.zsh
   source $SHELL_DIR/key-bindings.zsh
+elif [ -d "/usr/share/doc/fzf/examples/" ]; then
+  source /usr/share/doc/fzf/examples/completion.zsh
+  source /usr/share/doc/fzf/examples/key-bindings.zsh
 else
   FZF_DIR=$(dirname $(command -v fzf))/../share/fzf
   source $FZF_DIR/completion.zsh
