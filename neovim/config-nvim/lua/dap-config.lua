2c21aa8ff1ca9c15ecc84f31fa5db80a378a4c20
Alexander Bessonov <abessonov2@bloomberg.com>
2021-11-08 09:59:47 -0500

    Remove dap-python customization (the change is merged)

--- neovim/config-nvim/lua/dap-config.lua
+++ neovim/config-nvim/lua/dap-config.lua
@@ -1,27 +1,6 @@
 -- We're assuming debugpy package is installed with this python
 require('dap-python').setup('python')
 
--- Add my Config
-
-local python_configs = require('dap').configurations.python
-
-table.insert(python_configs, {
-  type = 'python';
-  request = 'launch';
-  name = 'Launch file with arguments';
-  program = '${workspaceFolder}/${file}';
-  args = function()
-    local args_string = vim.fn.input('Arguments: ')
-    local a = {}
-    for arg in string.gmatch(args_string, "%S+") do
-      table.insert(a, arg)
-    end
-    return a
-  end;
-  console = 'integratedTerminal';
-})
-
-
 vim.api.nvim_set_keymap('n', '<F5>', ":lua require('dap').continue()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<Leader>si', ":lua require('dap').step_into()<CR>", { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<Leader>so', ":lua require('dap').step_out()<CR>", { noremap = true, silent = true })
